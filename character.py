50e4f72804ef822887a13f9f911a8e51e9bccfa8
Chris Vasquez <chrvasq@outlook.com>
2019-05-26 12:52:49 -0700

    Implement Character class

--- character.py
+++ character.py
@@ -1,1 +1,18 @@
-# Create your Character class logic in here.
+class Character():
+    def __init__(self, char):
+        self.original = char
+        self.was_guessed = False
+
+    def compare_guess(self, guess):
+        if guess.lower() == self.original.lower():
+            self.was_guessed = True
+        else:
+            self.was_guessed = False
+        return self.was_guessed
+    
+    def show_original_character(self):
+        if self.was_guessed:
+            return self.original
+        else:
+            return '_'
+    
