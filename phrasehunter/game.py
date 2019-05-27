30ad56c3faff3a1d285e8d8fc7c9091ecb1aa373
Chris Vasquez <chrvasq@outlook.com>
2019-05-27 13:43:15 -0700

    Implement replay function in Game class and refactor get_user_input function

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -2,9 +2,11 @@ from .phrase import Phrase
 from random import choice
 from os import system, name, sys
 
+PHRASES = ['Time', 'Power', 'Soul', 'Reality', 'Space', 'Mind']
+
 
 class Game:
-    def __init__(self, phrases):
+    def __init__(self, phrases=PHRASES):
         self.phrases = [Phrase(phrase) for phrase in phrases]
         self.active_phrase = choice(self.phrases)
         self.lives = 5
@@ -12,7 +14,7 @@ class Game:
 
     def clear_screen(self):
         system('cls' if name == 'nt' else 'clear')
-    
+
     def welcome_message(self):
         welcome_message = ' Phrase Hunter '
         print('#' * (len(welcome_message) + 2))
@@ -20,19 +22,32 @@ class Game:
         print('#' * (len(welcome_message) + 2))
 
     def won_game(self):
-        print('You won!')
+        self.playing = False
+        print('You won! You guessed the phrase!')
         self.replay()
 
     def lost_game(self):
-        print('You lost!')
+        self.playing = False
+        print('You lost! Better luck next time!')
         self.replay()
 
     def replay(self):
-        pass
+        try:
+            user_input = input('Would you like to play again? Y/N ')
+            if user_input.lower() not in ['y', 'n']:
+                raise ValueError
+        except ValueError:
+            print('Please enter in \'Y\' or \'N\'.')
+            self.replay()
+        else:
+            if user_input.lower() == 'y':
+                game = Game()
+                game.main()
+            else:
+                print('Thanks for playing! See you next time!')
+                sys.exit()
 
     def get_user_input(self):
-        user_input = ''
-
         try:
             user_input = input('Guess a letter: ')
             if len(user_input) == 0:
@@ -45,30 +60,39 @@ class Game:
                 raise ValueError('Please enter one letter.')
         except ValueError as error:
             print(error)
+            self.get_user_input()
         else:
-            return user_input
+            if user_input.lower() in [letter.original.lower() for letter
+                                      in self.active_phrase]:
+                for letter in self.active_phrase:
+                    letter.compare_guess(user_input)
+                self.active_phrase.print_phrase()
+            else:
+                self.lives -= 1
+                print(f'You have {self.lives} out of 5 lives remaining!')
+                self.active_phrase.print_phrase()
 
-    def process_guess(self, guess=None):
-        while guess is None:
-            guess = self.get_user_input()
-        if guess.lower() in [letter.original.lower() for letter
-                             in self.active_phrase]:
-            for letter in self.active_phrase:
-                letter.compare_guess(guess)
-            self.active_phrase.print_phrase()
-        else:
-            self.lives -= 1
-            print(f'You have {self.lives} out of 5 lives remaining!')
-            self.active_phrase.print_phrase()
+    # def process_guess(self):
+    #     # while guess is None:
+    #     guess = self.get_user_input()
+    #     if guess.lower() in [letter.original.lower() for letter
+    #                          in self.active_phrase]:
+    #         for letter in self.active_phrase:
+    #             letter.compare_guess(guess)
+    #         self.active_phrase.print_phrase()
+    #     else:
+    #         self.lives -= 1
+    #         print(f'You have {self.lives} out of 5 lives remaining!')
+    #         self.active_phrase.print_phrase()
 
     def main(self):
+        self.clear_screen()
         self.welcome_message()
         self.active_phrase.print_phrase()
+
         while self.playing:
-            self.process_guess()
+            self.get_user_input()
             if self.active_phrase.phrase_guessed():
-                self.playing = False
                 self.won_game()
             if self.lives == 0:
-                self.playing = False
                 self.lost_game()
