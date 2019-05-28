95596270660423c83d8617b300e599950cacc0cf
Chris Vasquez <chrvasq@outlook.com>
2019-05-27 23:17:35 -0700

    PEP8 formatting for character.py

--- phrasehunter/character.py
+++ phrasehunter/character.py
@@ -5,7 +5,7 @@ END = '\033[0m'  # Reset formatting
 class Character:
     def __init__(self, char):
         """Instantiate Character object
-        
+
         Arguments:
             char {str} -- Single str character from A-Z (a-z)
         """
@@ -13,11 +13,12 @@ class Character:
         self.was_guessed = False
 
     def compare_guess(self, guess):
-        """Takes 'guess' as an argument and compares 'guess' to the Character object
-        
+        """Takes 'guess' as an argument and compares 'guess' to the
+        Character object
+
         Arguments:
             guess {str} -- Single str character from A-Z (a-z)
-        
+
         Returns:
             Bool -- Returns True
         """
@@ -26,8 +27,9 @@ class Character:
         return self.was_guessed
 
     def show(self):
-        """Dictates what to return based on self.was_guessed status of Character object
-        
+        """Dictates what to return based on self.was_guessed status
+        of Character object
+
         Returns:
             str -- Returns either '_' or self.original with formatting
         """
