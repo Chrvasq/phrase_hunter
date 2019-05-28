68100a338ecfbff51596eb87d602587eac9d309b
Chris Vasquez <chrvasq@outlook.com>
2019-05-27 22:08:29 -0700

    Add color formatting to error messages, win/lose messages, welcome messaging

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -3,6 +3,11 @@ from random import choice
 from os import system, name, sys
 
 PHRASES = ['Time', 'Power', 'Soul', 'Reality', 'Space', 'Mind']
+RED = '\033[1;91m'  # Bold and red
+YELLOW = '\033[1;93m'  # Bold and yellow
+GREEN = '\033[1;92m'  # Bold and green
+UNDERLINE = '\033[4m'  # Add underline
+END = '\033[0m'  # Reset formatting
 
 
 class Game:
@@ -20,43 +25,49 @@ class Game:
         print('#' * (len(welcome_message) + 2))
         print(f'#{welcome_message}#')
         print('#' * (len(welcome_message) + 2))
+        print(YELLOW + '\nWelcome to Phrase Hunter!\n' + END)
+        print(GREEN + UNDERLINE + 'Instructions' + END + ': Try to guess the '
+              'phrase by entering in a single letter from A-Z. '
+              'You have 5 lives.\n')
 
     def won_game(self):
         self.playing = False
-        print('You won! You guessed the phrase!')
+        print(YELLOW + 'You won! You guessed the phrase!\n' + END)
         self.replay()
 
     def lost_game(self):
         self.playing = False
-        print('You lost! Better luck next time!')
+        print(RED + 'You lost! Better luck next time!\n' + END)
         self.replay()
 
     def replay(self):
         try:
-            user_input = input('Would you like to play again? Y/N ')
+            user_input = input('Would you like to play again? (Y/N) > ')
             if user_input.lower() not in ['y', 'n']:
                 raise ValueError
         except ValueError:
-            print('Please enter in \'Y\' or \'N\'.')
+            print(YELLOW + 'Please enter in \'Y\' or \'N\'.\n' + END)
             self.replay()
         else:
             if user_input.lower() == 'y':
                 return Game().main()
             else:
-                print('Thanks for playing! See you next time!\n')
+                print(YELLOW + '\nThanks for playing! See you '
+                      'next time!\n' + END)
                 sys.exit()
 
     def get_user_input(self):
         try:
             user_input = input('Guess a letter: ')
+            print('\n')
             if len(user_input) == 0:
-                raise ValueError('You didn\'t enter a letter. '
-                                 'Please enter a letter between A-Z')
+                raise ValueError(YELLOW + 'You didn\'t enter a letter. '
+                                 'Please enter a letter between A-Z\n' + END)
             if not user_input.isalpha():
-                raise ValueError('You entered a number. '
-                                 'Please enter a letter between A-Z.')
+                raise ValueError(YELLOW + 'You entered a number. '
+                                 'Please enter a letter between A-Z.\n' + END)
             if len(user_input) > 1:
-                raise ValueError('Please enter one letter.')
+                raise ValueError(YELLOW + 'Please enter one letter.\n' + END)
         except ValueError as error:
             print(error)
             self.get_user_input()
@@ -68,7 +79,7 @@ class Game:
                 self.active_phrase.print_phrase()
             else:
                 self.lives -= 1
-                print(f'You have {self.lives} out of 5 lives remaining!')
+                print(f'You have {self.lives} out of 5 lives remaining!\n')
                 self.active_phrase.print_phrase()
 
     def main(self):
